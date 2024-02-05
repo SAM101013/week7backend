99dd6c5206bc67224d2b90ed0c85a6e2a3e49ed3
SAM101013 <amoonmarketing@proton.me>
2024-02-05 14:47:59 +0000

    enhancement

--- src/server.js
+++ src/server.js
@@ -2,10 +2,20 @@ const express = require("express");
 
 const app = express();
 
-app.get("/health", (req, res) => {
-  res.send("healthy");
-});
+app.use("/example", express.static("example"));
+
+//app.use is an object method/example is the argument passed
+//example is the filename where html data files live
+//this will call the index data within the "example file"
+
+// app.get("/health", (req, res) => {
+//   res.send("healthy");
+// });
+
+//the health route makes a get request to check health of the server
 
 app.listen(5001, () => {
   console.log("Server is listening on port 5001");
 });
+
+//creating the get request to check server is listening
